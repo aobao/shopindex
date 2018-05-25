56a2433fb73cc82aab1cd02736ee80f2ad731543
wangshijie <1571026979@qq.com>
2018-05-25 11:50:35 +0800

    王
    王

--- src/components/common/detail.vue
+++ src/components/common/detail.vue
@@ -2,13 +2,67 @@
 <div class="main">
   <div class="mytopbox">
    <div class="mylist">
-     <div class="img"></div>
+     <div class="all">
+         <img src="../../assets/img/allproduct.png" alt="">
+     </div>
+     <div class="type">
+         <div class="img">
+         <img src="../../assets/img/bg-green.png" alt="">             
+         </div>
+         <span>鸡尾酒</span>
+     </div>
+     <div class="gray">
+         <div class="img">
+         <img src="../../assets/img/gray.png" alt="">
+
+         </div>
+         <span>产品详情</span>
+     </div>
    </div>
    <div class="detail">
-       <div class="left"></div>
-       <div class="right"></div>
+    <div class="left">
+           <div class="imgbox">
+            <img src="../../assets/img/detail-jiu.png" alt="">        
+            </div>  
+    </div>
+       <div class="right">
+           <h1>经典炫彩鸡尾酒套餐预调果味酒 <span>275</span>ml* <span>6</span> 瓶 </h1>
+           <h2>开盖有奖，每一口都是甜甜果香 </h2>
+           <div class="price">
+               <h1><span>576</span> RMB     <s>原价 <span>987</span>RMB </s> </h1>
+           </div>
+           <div class="mun">
+               <h1>
+                 <span>清选择商品数量</span>
+                 <span @click='munred()'>-</span>
+                  <div calss='def'>{{mun}}</div>
+                  <span @click='munadd()'>+</span>
+               </h1>               
+           </div>
+           <div class="type">
+                <div class="left">
+                    <h2>请选择商品口味</h2>
+                </div>
+                <div class="type-right">
+                  <div class="typebox" >
+                    <div class="mytype" v-for='(item,n) in typedata' @click='index=n' :key='n' :class='{active:index==n}'>
+                        <div class="imgbox"><img :src="item.src" alt=""></div> 
+                       <div class="mylike">{{item.type}}</div>
+                        
+                    </div>
+                  </div>
+                </div>
+           </div>
+           <div class="addbtn">
+               立即购买
+           </div>
+           <div class="shopbtn">
+               加入购物车
+           </div>
+       </div>
    </div>
   </div>  
+   
   <div class="max">
    <div class="detail">
       <div class="img">
@@ -17,12 +71,65 @@
       <span>商品详情</span>
    </div>
   </div>
+
+  <div class="imgdetail">
+    <div class="imgbox">
+      <img src="../../assets/img/omygaga.png" alt="">
+    </div>
+    
+  </div>
 </div>
 </template>
+
 <script>
-   
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
 export default {
-    
+    data(){
+        return{
+            mun:1,
+            typedata:[
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                {src:'./static/img/detail-jiu.png',type:'葡萄口味'},
+                
+            ] ,
+            bannerdata:[
+                {src:'../static/img/detail-jiu.png'},
+                {src:'../static/img/detail-jiu.png'},
+                {src:'../static/img/detail-jiu.png'},
+                {src:'../static/img/detail-jiu.png'},
+                
+            ],
+            index:0,
+           swiperOption: {
+                navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+                }
+            }
+        }
+        
+    },
+     mounted() {
+   
+    },
+    methods:{
+        munred(){
+            if(this.mun<0){
+               this.mun=0;
+            }
+            this.mun--;
+        },
+        munadd(){
+            this.mun++;
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
@@ -37,19 +144,216 @@ export default {
         .mylist{
             width: 100%;
             height: 100px;
+            .all{
+                float: left;
+                width: 100px;
+                height: 50px;
+                margin-top:25px;
+            }
+            .type{
+                float:left;
+                position: relative;
+                width: 100px;
+                height: 50px;
+                margin-top: 25px;
+                .img{
+                    position: absolute;
+                    top: 0;
+                    left:0;
+                    width: 100px;
+                    height: 100%;
+                    img{
+                        width: 100%;
+                    }
+                }
+                span{
+                    position: absolute;
+                    top:4px;
+                    left:10px;
+                    color:#fff;
+                    font-size: 12px;
+                }
+            }
+            .gray{
+                float:left;
+                position: relative;
+                width: 100px;
+                height: 50px;
+                margin-top: 25px;
+                margin-left: 10px;
+                .img{
+                    position: absolute;
+                    top: 0;
+                    left:0;
+                    width: 100px;
+                    height: 100%;
+                    img{
+                        width: 100%;
+                    }
+                }
+                span{
+                    position: absolute;
+                    top:4px;
+                    left:10px;
+                    color:#fff;
+                    font-size: 12px;
+                }
+
+            }
         }
         .detail{
             width: 100%;
             position: relative;
             height: 410px;
-            .left{
-              
+            >.left{
+              width: 400px;
+              height: 400px;
+              position: absolute;
+              top:-30px;
+              left:-40px;
+              z-index: 100;
+              .imgbox{
+                  width: 100%;
+                  height: 100%;
+                  position: relative;
+                 img{
+                     width: 100%;
+                 }
+               
+              }
+                
             }
             .right{
-                width: 675px;
+                position: absolute;
+                width: 375px;
                 height: 410px;
                 right:0;
                 top:0;
+                padding-left:200px;
+                padding-right:100px;
+                background-color: #fff;
+                h1{
+                    margin-top:40px;
+                    font-size: 16px;
+                }
+                h2{
+                    margin-top:12px;
+                    font-size: 12px;
+                    color:#8D8D8D;
+                }
+                .price{
+                    h1{
+                        font-size:24px;
+                        color:#FF5D5D;
+                        span{
+                            font-size: 24px;
+                        }
+                        s{
+                            font-size: 18px;
+                            color:#8D8D8D;
+                            span{
+                                font-size: 18px;
+                            }
+                        }
+                    }
+                }
+                .mun{
+                    width: 200px;
+                    span{
+                        cursor: pointer;
+                    }
+                    h1{
+                        font-size:16px;
+                        display: flex;
+                        justify-content: space-between;
+                        margin-top: 20px;
+                        color:#B0B0B0;
+                        div.def{
+                            width: 60px;
+                            height: 10px;
+                            border-bottom:1px solid #888;
+                        }
+                    }
+                }
+                .type{
+                    position: relative;
+                    width: 375px;
+                    height: 120px;
+                    cursor: pointer;
+                    .left{
+                        width: 112px;
+                        height: 30px;
+                        float: left;
+                        h2{
+                            font-size: 16px;
+                        }
+                    }
+                    .type-right{
+                        width: 280px;
+                        height: 120px;
+                        float:right;
+                        margin-right:-30px;  
+                        .typebox{
+                            width: 100%;
+                            height: 100%;
+                            display: flex;
+                            justify-content: space-between;
+                            align-items: center;
+                            flex-wrap: wrap;
+                            .mytype{
+                                width: 86px;
+                                height: 30px;
+                                display: flex;
+                                justify-content: space-around;
+                                align-items: center;
+                                border:1px solid #888;
+                                &.active{
+                                    background-color: #b9e6e0;
+                                }
+                                div.imgbox{
+                                    width: 20px;
+                                    height: 20px;
+                                    img{
+                                        width: 100%;
+                                    }
+                                }
+                                div.mylike{
+                                    width: 60px;
+                                    height: 12px;
+                                    font-size: 12px;
+                                }
+                            } 
+                        }
+                    }
+                }
+                .addbtn{
+                    width: 140px;
+                    height: 40px;
+                    position: absolute;
+                    left:220px;
+                    bottom:20px;
+                    background-color: #B0B1C3;
+                    color:#fff;
+                    font-size: 18px;
+                    line-height: 40px;
+                    text-align: center;
+                    cursor: pointer;
+                }
+                .shopbtn{
+                    width: 140px;
+                    height: 40px;
+                    position: absolute;
+                    left:370px;
+                    bottom:20px;
+                    background-color: #fff;
+                    box-sizing: border-box;
+                    border:2px solid #B0B1C3;
+                    color:#000;
+                    font-size: 18px;
+                    line-height: 36px;
+                    text-align: center;
+                    cursor: pointer;
+                } 
             }
         }
      }
@@ -58,6 +362,7 @@ export default {
          height: 38px;
          padding-top:12px;
          background: #b0b1c3;
+         margin-bottom: 100px;
          .detail{
              position: relative;
              width: 200px;
@@ -84,6 +389,19 @@ export default {
             }
          }
          
+     }
+     .imgdetail{
+         width: 855px;
+         height: auto;
+         margin:0 auto;
+        >.imgbox{
+            width: 100%;
+            // height: 800px;
+            position: relative;
+            img{
+                width: 100%
+            }
+        }  
      }
  }
 </style>
